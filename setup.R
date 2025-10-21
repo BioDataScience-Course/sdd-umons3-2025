67ce0828d205c9790472d670567ee1d90e985017
Philippe Grosjean <phgrosjean@sciviews.org>
2025-10-21 10:47:06 +0200

    Individual assignment module 3

--- setup.R
+++ setup.R
@@ -76,7 +76,7 @@ learnitdown$assign_url <- list(
   C01Ib_lda            = "https://classroom.github.com/a/KebgqQXM",
   C01Ga_metrics        = "https://classroom.github.com/a/6YLX7ccV",
   C02Ia_zoo            = "https://classroom.github.com/a/3viLYZaN",
-  C03Ia_cardiovascular = "https://classroom.github.com/a/...",
+  C03Ia_cardiovascular = "https://classroom.github.com/a/3p2xDMRm",
   C03Ca_ml_wine        = "https://classroom.github.com/a/...",
   C04Ia_ts             = "https://classroom.github.com/a/...",
   C04Ga_tseries        = "https://classroom.github.com/a/...",
